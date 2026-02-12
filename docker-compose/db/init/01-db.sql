62780b4ffe5a0167b574c5293922fc3cb54ad51c
Donatas <ktp211zdv@yandex.ru>
2026-02-12 22:07:24 +0300

    sub

--- docker-compose/db/init/01-db.sql
+++ docker-compose/db/init/01-db.sql
@@ -8,8 +8,14 @@ CREATE DATABASE georesolve
        LC_CTYPE = 'C.UTF-8'
        TEMPLATE = template0;
 
+CREATE DATABASE subscribe
+  WITH ENCODING = 'UTF8'
+       LC_COLLATE = 'C.UTF-8'
+       LC_CTYPE = 'C.UTF-8'
+       TEMPLATE = template0;
 -- Роль для базы georesolve
 CREATE ROLE georesolv_user WITH LOGIN PASSWORD 'georesolv_password';
+CREATE ROLE subscribe_user WITH LOGIN PASSWORD 'subscribe_password';
 
 -- PostGIS только в georesolve
 \c georesolve
@@ -32,11 +38,17 @@ CREATE INDEX cities_name_idx ON cities (name);
 CREATE INDEX cities_region_idx ON cities (region);
 CREATE INDEX cities_federal_district_idx ON cities (federal_district);
 
+
 -- Выдаём права пользователю georesolv_user на таблицу cities
 GRANT SELECT ON cities TO georesolv_user;
 GRANT USAGE, SELECT ON SEQUENCE cities_id_seq TO georesolv_user;
 
--- Владелец базы georesolve — georesolv_user
+\c subscribe
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+GRANT USAGE, CREATE ON SCHEMA public TO subscribe_user;
+
+-- Владельцы баз
 \c postgres
 ALTER DATABASE georesolve OWNER TO georesolv_user;
+ALTER DATABASE subscribe OWNER TO subscribe_user;
 
